f11b4a81aa935e33bb0e6c6b7f6abe0b759f8257
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2023-05-13 22:05:18 +0300

    add final less

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,14 @@ import { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Layout from './Layout/Layout'
+<<<<<<< Updated upstream
 import RegistrationPage from './pages/RegistrationPage'
 import { useSelector } from 'react-redux'
 import { Toaster } from 'react-hot-toast'
+=======
+import PrivateRoute from './PrivateRoute/PrivateRoute'
+import PublicRoute from './PublicRoute/PublicRoute'
+>>>>>>> Stashed changes
 
 const LoginPage = lazy(() => import('./pages/LoginPage'))
 const ToDoDetails = lazy(() => import('./ToDo/ToDoDetails'))
@@ -26,27 +31,58 @@ const App = () => {
 
 			<Routes>
 				<Route path='/' element={<Layout />}>
-					<Route index element={<HomePage />} />
-					<Route path='news' element={<NewsPage />} />
-
-					<Route path='todo' element={<ToDoPage />} />
-					<Route path='todo/:id' element={<ToDoDetails />} />
-					<Route path='products' element={<ProductsPage />} />
+					<Route index element={<HomePage />} />{' '}
+					<Route
+						path='news'
+						element={
+							<PrivateRoute>
+								<NewsPage />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path='todo'
+						element={
+							<PrivateRoute>
+								<ToDoPage />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path='todo/:id'
+						element={
+							<PrivateRoute>
+								<ToDoDetails />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path='products'
+						element={
+							<PrivateRoute>
+								<ProductsPage />
+							</PrivateRoute>
+						}
+					/>
 				</Route>
 				<Route
 					path='/login'
 					element={
-						<Suspense>
-							<LoginPage />
-						</Suspense>
+						<PublicRoute>
+							<Suspense>
+								<LoginPage />
+							</Suspense>
+						</PublicRoute>
 					}
 				/>
 				<Route
 					path='/signUp'
 					element={
-						<Suspense>
-							<RegistrationPage />
-						</Suspense>
+						<PublicRoute>
+							<Suspense>
+								<RegistrationPage />
+							</Suspense>
+						</PublicRoute>
 					}
 				/>
 			</Routes>
